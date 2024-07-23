7f286bf4e9a2c01c2bad9e14ab4007a57731972d
Houdaifa Lamrini <houdaifalamrini@MacBook-Air-de-Houdaifa.local>
2024-07-23 19:38:55 +0200

    caca

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./pronoms.html",
+    "./index.html",
     "./verbes.html",
+    "./adverbes.html",
     "./adjectifs.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
